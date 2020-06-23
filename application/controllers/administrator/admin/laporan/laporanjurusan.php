ce716dc5194b658ffe5410b11bacee9374e73896
edya rosadi <edya.rosadi@gmail.com>
2020-06-23 22:02:44 +0800

    add filter on report jurusan

--- application/controllers/administrator/admin/laporan/laporanjurusan.php
+++ application/controllers/administrator/admin/laporan/laporanjurusan.php
@@ -1,24 +1,40 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Laporanjurusan extends CI_Controller {
+class Laporanjurusan extends CI_Controller
+{
+    public function index()
+    {
+        //ambil nilai yang di post
+        $filter   = $this->input->post('filter');
+        $jurusan  = $this->input->post('jurusan');
+        $kelas    = $this->input->post('kelas');
 
- public function index()
- {
-  $data['tb_tambahjurusan'] = $this->db->get('tb_tambahjurusan')->result();
-  $this->load->view('administrator/admin/laporan/filter', $data);  
- }
- public function filter($id)
- 	{
-  if ($id == 0) {
-   $data = $this->db->get('tb_jrs')->result();
-  }
-  else
-  {
-   $data = $this->db->get_where('tb_jrs', ['jurusan'=>$id])->result();
-  }
-  $dt['jrs'] = $data;
-  $dt['jurusan'] = $id;
-  $this->load->view('administrator/admin/laporan/result', $dt);
- }
+        //cek nilai filter untuk dihubungkan dengan model
+        if ($filter == '1') {
+            $data['jurusan'] = $this->jurusan_model->view_by_jurusan($jurusan);
+        } elseif ($filter == '2') {
+            $data['jurusan'] = $this->jurusan_model->view_by_kelas($kelas);
+        } elseif ($filter == '3') {
+            $data['jurusan'] = $this->jurusan_model->view_by_jurusankelas($jurusan, $kelas);
+        } else { // Jika user tidak mengklik tombol tampilkan
+            $data['jurusan'] = $this->jurusan_model->view_all(); // Panggil fungsi view_all yang ada di TransaksiModel
+        }
+
+        //kirimkan data yang diambil dari model ke view laporan
+        $this->load->library('mypdf');
+        $this->mypdf->generate('administrator/laporan/print_jurusan', $data);
+    }
+
+    // public function filter($id)
+    // {
+    //     if ($id == 0) {
+    //         $data = $this->db->get('tb_jrs')->result();
+    //     } else {
+    //         $data = $this->db->get_where('tb_jrs', ['jurusan' => $id])->result();
+    //     }
+    //     $dt['jrs'] = $data;
+    //     $dt['jurusan'] = $id;
+    //     $this->load->view('administrator/admin/laporan/result', $dt);
+    // }
 }
--- application/views/administrator/admin/jurusan.php
+++ application/views/administrator/admin/jurusan.php
@@ -1,75 +1,76 @@
 <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css">  -->
- <!-- Load file css jquery-ui -->
+<!-- Load file css jquery-ui -->
 
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Jurusan</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo base_url() ?>administrator/admin/dashboardadmin">Home</a></li>
-              <li class="breadcrumb-item active">Jurusan</li>
-            </ol>
-          </div>
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Jurusan</h1>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="<?php echo base_url() ?>administrator/admin/dashboardadmin">Home</a></li>
+            <li class="breadcrumb-item active">Jurusan</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
 
   <section class="content">
-      <div class="row">
-        <div class="col-12">
-    <div class="card">
-            <div class="card-header"> 
-              <h3 class="card-title">Daftar Jurusan</h3>
-            </div>
-              <?php echo $this->session->flashdata('pesan') ?>
-              <div class="card-body">
-                <?php echo anchor('administrator/admin/jurusan/input','<button class="btn btn-sm btn-primary mb-3"><i class="fas fa-plus fa-mr"></i> Tambah Jurusan </button>') ?>
-                <!-- <?php echo anchor('administrator/admin/jurusan/print','<button class="btn btn-sm btn-success mb-3"><i class="fas fa-print"></i> Print </button>') ?> -->
-        
-        <form method="get" action="">
-                <div class="row">
-                  <div class="col-2 ">
-                    <label>Filter Berdasarkan</label><br>
-                    <select name="filter" id="filter" class="form-control custom-select">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Daftar Jurusan</h3>
+          </div>
+          <?php echo $this->session->flashdata('pesan') ?>
+          <div class="card-body">
+            <?php echo anchor('administrator/admin/jurusan/input', '<button class="btn btn-sm btn-primary mb-3"><i class="fas fa-plus fa-mr"></i> Tambah Jurusan </button>') ?>
+            <!-- <?php echo anchor('administrator/admin/jurusan/print', '<button class="btn btn-sm btn-success mb-3"><i class="fas fa-print"></i> Print </button>') ?> -->
+
+            <form method="post" action="<?php echo base_url('administrator/admin/laporan/laporanjurusan') ?>">
+              <div class="row">
+                <div class="col-2 ">
+                  <label>Filter Berdasarkan</label><br>
+                  <select name="filter" id="filter" class="form-control custom-select">
                     <option value="">Pilih</option>
                     <option value="1">Per Jurusan</option>
                     <option value="2">Per Kelas</option>
-                    </select>
-                  </div>
-
-                  <div id="form-jurusan" class="col-3">
-                  <label>Jurusan</label>
-                  <select name="jurusan" class="form-control custom-select" data-placeholder="-Pilih-" style="width: 100%;">
-                  <option selected disabled>-Pilih-</option>
-                  <?php foreach ($tb_jrs as $jr) : ?>
-                    <option value="<?php echo $jr->jurusan ?>"><?php echo $jr->jurusan; ?></option>
-                  <?php endforeach; ?>
+                    <option value="3">Per Jurusan dan Kelas</option>
                   </select>
                 </div>
 
-                  <div id="form-kelas" class="col-3">
-            <label>Kelas</label>
-            <select name="kelas" class="form-control custom-select" data-placeholder="-Pilih-" style="width: 100%;">
-                  <option selected disabled>-Pilih-</option>
-                  <?php foreach ($tb_kelas as $kl) : ?>
-                    <option value="<?php echo $kl->kelas ?>"><?php echo $kl->kelas; ?></option>
-                <?php endforeach; ?>
-            </select>
-           </div>
+                <div id="form-jurusan" class="col-3">
+                  <label>Jurusan</label>
+                  <select name="jurusan" class="form-control custom-select" data-placeholder="-Pilih-" style="width: 100%;">
+                    <option selected disabled>-Pilih-</option>
+                    <?php foreach ($tb_jrs as $jr) : ?>
+                      <option value="<?php echo $jr->jurusan ?>"><?php echo $jr->jurusan; ?></option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
 
-                </div><br>
-                <button type="submit" class="btn btn-sm btn-success mb-4">Tampilkan</button>
-                                   
-    </form>
+                <div id="form-kelas" class="col-3">
+                  <label>Kelas</label>
+                  <select name="kelas" class="form-control custom-select" data-placeholder="-Pilih-" style="width: 100%;">
+                    <option selected disabled>-Pilih-</option>
+                    <?php foreach ($tb_kelas as $kl) : ?>
+                      <option value="<?php echo $kl->kelas ?>"><?php echo $kl->kelas; ?></option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
 
-          <table id="example1" class="table table-bordered table-striped table-hover">    
-                <thead>
+              </div><br>
+              <button type="submit" name="cetak" class="btn btn-sm btn-success mb-4">Tampilkan</button>
+
+            </form>
+
+            <table id="example1" class="table table-bordered table-striped table-hover">
+              <thead>
                 <tr>
                   <th width="20px">NO</th>
                   <th width="49px">NIS</th>
@@ -80,77 +81,70 @@
                   <th width="100px">Tahun Ajaran</th>
                   <th width="50px">AKSI</th>
                 </tr>
-                </thead>
-                <tbody>               
+              </thead>
+              <tbody>
                 <?php
                 $no = 1;
                 foreach ($tb_jrs as $jr) : ?>
-                
-                 <tr>
-                        <td><?php echo $no++ ?></td>
-                        <td><?php echo $jr->nis?></td>
-                        <td><?php echo $jr->nama_siswa?></td>
-                        <td><?php echo $jr->jurusan?></td>
-                        <td><?php echo $jr->kelas?></td>
-                        <td><?php echo $jr->semester?></td>   
-                        <td><?php echo $jr->thn_ajaran?></td>
-                        <td class="project-actions text-left">                    
-                          <a width="20px"><?php echo anchor('administrator/admin/jurusan/update/'.$jr->id_jrs,'<div class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></div>') ?>
-                        </a>
-                          <a width="20px"><?php echo anchor('administrator/admin/jurusan/delete/'.$jr->id_jrs,'<div class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></div>') ?>
-                          </a>
-                      </td>
+
+                  <tr>
+                    <td><?php echo $no++ ?></td>
+                    <td><?php echo $jr->nis ?></td>
+                    <td><?php echo $jr->nama_siswa ?></td>
+                    <td><?php echo $jr->jurusan ?></td>
+                    <td><?php echo $jr->kelas ?></td>
+                    <td><?php echo $jr->semester ?></td>
+                    <td><?php echo $jr->thn_ajaran ?></td>
+                    <td class="project-actions text-left">
+                      <a width="20px"><?php echo anchor('administrator/admin/jurusan/update/' . $jr->id_jrs, '<div class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></div>') ?>
+                      </a>
+                      <a width="20px"><?php echo anchor('administrator/admin/jurusan/delete/' . $jr->id_jrs, '<div class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></div>') ?>
+                      </a>
+                    </td>
                   </tr>
                 <?php endforeach ?>
-                </tbody>
-                <tfoot>
-              </table>
-            </div>
-            <!-- /.card-body -->
+              </tbody>
+              <tfoot>
+            </table>
           </div>
-          <!-- /.card -->
+          <!-- /.card-body -->
         </div>
-        <!-- /.col -->
+        <!-- /.card -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
 </div>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
- -->
-<script src="<?php echo base_url('assets/jquery.min.js'); ?>"></script>
- <script>
-    $(document).ready(function(){
-       $('#form-jurusan, #form-kelas').hide();
-        $('#filter').change(function(){ // Ketika user memilih filter
-            if($(this).val() == '1'){ // Jika filter nya 1 (per tanggal)
-                $('#form-jurusan, #form-kelas').show(); // Sembunyikan form bulan dan tahun
-            }else if($(this).val() == '2'){ // Jika filter nya 2 (per bulan)
-                $('#form-jurusan').hide(); // Sembunyikan form tanggal
-                $('#form-kelas').show(); // Tampilkan form bulan dan tahun
-            }
+<!-- /.content-wrapper -->
 
-            $('#form-jurusan select, #form-kelas select').val(''); // Clear data pada textbox tanggal, combobox bulan & tahun
-        });
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+
+<script src="<?php echo base_url('assets/jquery.min.js'); ?>"></script>
+<script>
+  $(document).ready(function() {
+    $('#form-jurusan, #form-kelas').hide();
+    $('#filter').change(function() { // Ketika user memilih filter
+      if ($(this).val() == '1') { // Jika filter nya 1 (per tanggal)
+        $('#form-jurusan').show(); // Sembunyikan form tanggal
+        $('#form-kelas').hide(); // Tampilkan form bulan dan tahun
+      } else if ($(this).val() == '2') { // Jika filter nya 2 (per bulan)
+        $('#form-jurusan').hide(); // Sembunyikan form tanggal
+        $('#form-kelas').show(); // Tampilkan form bulan dan tahun
+      } else if ($(this).val() == '3') { // Jika filter nya 1 (per tanggal)
+        $('#form-jurusan, #form-kelas').show(); // Sembunyikan form bulan dan tahun
+      }
+
+      $('#form-jurusan select, #form-kelas select').val(''); // Clear data pada textbox tanggal, combobox bulan & tahun
     });
-    </script>
+  });
+</script>
 
 
 <!-- <script type="text/javascript"> 
